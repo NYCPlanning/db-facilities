492d2beff092fecfe0b96d8bd1f668def9c65398
Molly Graber <molly.graber@me.com>
2021-05-10 14:24:19 -0400

    Create facdb_address (#403)
    * Create facdb_address
    
    * Fix into and from order
    
    * dcp_colp needs special handling
    
    * adding code execution to the cli
    
    Co-authored-by: sptkl <caobaiyue@gmail.com>

---
+++ facdb/sql/_create_facdb_address.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS facdb_address;
+SELECT
+	uid,
+	source,
+	(CASE WHEN source = 'dcp_colp' then addressnum else geo_house_number END) as addressnum,
+	(CASE WHEN source = 'dcp_colp' then streetname else geo_street_name END) as streetname,
+	UPPER(CASE
+		WHEN source = 'dcp_colp' then address
+		WHEN geo_grc in ('00', '01') and geo_grc2 in ('00', '01')
+		THEN nullif(geo_house_number||' '||geo_street_name,'')
+	ELSE address END) as address
+INTO facdb_address
+FROM (
+	SELECT
+        uid,
+        source,
+        addressnum,
+        streetname,
+        nullif(geo_1b->'result'->>'geo_house_number','') as geo_house_number,
+        nullif(geo_1b->'result'->>'geo_street_name','') as geo_street_name,
+        nullif(geo_1b->'result'->>'geo_grc','') as geo_grc,
+        nullif(geo_1b->'result'->>'geo_grc2','') as geo_grc2,
+        geo_1b->'inputs'->>'input_hnum' as input_hnum,
+        geo_1b->'inputs'->>'input_sname' as input_sname,
+        address
+    FROM facdb_base
+) a;
