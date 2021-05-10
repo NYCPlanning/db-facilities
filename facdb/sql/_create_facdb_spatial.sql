d9261304b1a9598b41c3f6ddb1334ecadeac784e
sptkl <caobaiyue@gmail.com>
2021-05-10 17:19:01 +0000

    remove borocode inconsistencies

--- facdb/sql/_create_facdb_spatial.sql
+++ facdb/sql/_create_facdb_spatial.sql
@@ -5,7 +5,7 @@ with boundary_geosupport as (
 		nullif(nullif(geo_1b->'result'->>'geo_borough_code',''), '0')::integer as borocode,
 		nullif(geo_1b->'result'->>'geo_zip_code','') as zipcode,
 		nullif(geo_1b->'result'->>'geo_bin','') as bin,
-		nullif(geo_1b->'result'->>'geo_bbl','') as bbl,
+		nullif(nullif(geo_1b->'result'->>'geo_bbl',''), '0000000000') as bbl,
 		nullif(geo_1b->'result'->>'geo_city','') as city,
 		nullif(geo_1b->'result'->>'geo_commboard','') as commboard,
 		nullif(geo_1b->'result'->>'geo_nta','') as nta,
@@ -20,7 +20,10 @@ with boundary_geosupport as (
 ), boundary_spatial_join as (
 	SELECT
 		uid,
-		(select borocode from dcp_boroboundaries_wi b where st_intersects(b.wkb_geometry, a.geom)) as borocode,
+		coalesce(
+			(select left(bbl::text, 1)::integer from dcp_mappluto b where st_intersects(b.wkb_geometry, a.geom)),
+			(select borocode::integer from dcp_boroboundaries_wi b where st_intersects(b.wkb_geometry, a.geom))
+		)as borocode,
 		(select zipcode from doitt_zipcodeboundaries b where st_intersects(b.wkb_geometry, a.geom) limit 1) as zipcode,
 		(select bin from doitt_buildingfootprints b where st_intersects(b.wkb_geometry, a.geom)) as bin,
 		(select bbl::bigint::text from dcp_mappluto b where st_intersects(b.wkb_geometry, a.geom)) as bbl,
