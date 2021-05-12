fc39d238a1b00b8f8c0eb457a18f3049cafc5db2
sptkl <caobaiyue@gmail.com>
2021-05-12 15:39:29 +0000

    adding export to github actions

--- facdb/bash/export.sh
+++ facdb/bash/export.sh
@@ -6,6 +6,17 @@ max_bg_procs 5
 mkdir -p output && (
     cd output
     echo "*" > .gitignore
-    CSV_export facdb facilities
-    SHP_export facdb POINT
+    CSV_export facdb facilities &
+    CSV_export qc_operator &
+    CSV_export qc_oversight &
+    CSV_export qc_classification &
+    CSV_export qc_captype &
+    CSV_export qc_proptype &
+    CSV_export qc_mapped &
+    CSV_export qc_diff &
+    CSV_export geo_rejects &
+    CSV_export geo_result &
+    SHP_export facdb POINT facilities
+    wait
+    echo "export complete"
 )
