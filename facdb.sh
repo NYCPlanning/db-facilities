dcbc8ee7364b72c2a14954bf2310c3f8a5be7c8d
sptkl <caobaiyue@gmail.com>
2021-05-12 15:50:18 +0000

    adding upload artifacts

--- facdb.sh
+++ facdb.sh
@@ -8,7 +8,18 @@ function facdb_execute {
     docker-compose exec -T facdb facdb $@
 }
 
+function facdb_upload {
+    local branchname=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+    local DATE=$(date "+%Y-%m-%d")
+    local PATH=edm-publishing/db-facilities/$branchname
+    mc rm -r --force spaces/$PATH/latest
+    mc rm -r --force spaces/$PATH/$DATE
+    mc cp -r output spaces/$PATH/latest
+    mc cp -r output spaces/$PATH/$DATE
+}
+
 case $1 in
     init) init ;;
-    *) facdb_execute $@;;
+    upload) facdb_upload ;;
+    *) facdb_execute $@ ;;
 esac
