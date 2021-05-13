caba7ca4bc33525b9e261b6362c43be536e94d6d
mgraber <mgraber@planning.nyc.gov>
2021-05-13 14:54:11 +0000

    Move overagency join to facdb_agency

--- facdb/sql/_create_facdb_agency.sql
+++ facdb/sql/_create_facdb_agency.sql
@@ -1,16 +1,37 @@
 DROP TABLE IF EXISTS facdb_agency;
-SELECT
-	a.*, b.overagency, b.overlevel
-INTO facdb_agency
-FROM (
+WITH
+op_join AS (
 	SELECT
-		uid,
+		a.uid,
 		a.opname,
 		a.opabbrev,
 		b.optype,
-		a.overabbrev
+		b.overabbrev,
+		b.overagency,
+		b.overlevel
 	FROM facdb_base a
 	LEFT JOIN lookup_agency b
 	ON a.opabbrev = b.opabbrev
-) a LEFT JOIN lookup_agency b
-ON a.overabbrev = b.overabbrev
+),
+over_join AS (
+	SELECT
+		a.uid,
+		a.overabbrev,
+		b.overagency,
+		b.overlevel
+	FROM facdb_base a
+	LEFT JOIN lookup_agency b
+	ON a.overabbrev = b.overabbrev
+)
+SELECT
+	a.uid,
+	a.opname,
+	a.opabbrev,
+	a.optype,
+	COALESCE(a.overabbrev, b.overabbrev) as overabbrev,
+	COALESCE(a.overagency, b.overagency) as overagency,
+	COALESCE(a.overlevel, b.overlevel) as overlevel
+INTO facdb_agency
+FROM op_join a
+JOIN over_join b
+ON a.uid = b.uid;
