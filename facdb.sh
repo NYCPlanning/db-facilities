6a563c8c645b4f9471d93c95ee78be7e30d4d5e9
sptkl <caobaiyue@gmail.com>
2021-05-14 15:18:59 +0000

    adding steps to archive

--- facdb.sh
+++ facdb.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+source facdb/bash/config.sh
+max_bg_procs 5
+
 function init {
     docker-compose up -d
     docker-compose exec -T facdb facdb init
@@ -18,8 +21,27 @@ function facdb_upload {
     mc cp -r output $SPACES/$DATE
 }
 
+function facdb_archive {
+    shift;
+    case $1 in
+    --all)
+        archive public.facdb facdb.facdb &
+        archive public.facdb_base facdb.facdb_base &
+        archive public.facdb_geom facdb.facdb_geom &
+        archive public.facdb_spatial facdb.facdb_spatial
+        archive public.facdb_agency facdb.facdb_agency &
+        archive public.facdb_address facdb.facdb_address &
+        archive public.facdb_classification facdb.facdb_classification
+        wait
+        echo "Archive Complete"
+        ;;
+    *) archive $@ ;;
+    esac
+}
+
 case $1 in
     init) init ;;
     upload) facdb_upload ;;
+    archive) facdb_archive $@ ;;
     *) facdb_execute $@ ;;
 esac
