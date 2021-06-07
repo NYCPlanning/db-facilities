873005b781fdb0be59c9c43325a7cd7416cb174b
SPTKL <caobaiyue@gmail.com>
2021-06-07 11:09:35 -0400

    nycha community center deduplication (#472)
    * nycha community center deduplication
    
    * adding command to cli.py
    
    * clear out million bins and do not allow deduplication for null bins
    
    * adding borocode
    
    * #473 upper facname and trim

---
+++ facdb/sql/_deduplication.sql
@@ -0,0 +1,21 @@
+-- For factype NYCHA COMMUNITY CENTER - CHILD CARE,
+-- if dohmh_daycare has a site with the same BIN, delete the nycha record
+DELETE FROM facdb
+WHERE factype = 'NYCHA COMMUNITY CENTER - CHILD CARE'
+AND datasource = 'nycha_communitycenters'
+AND bin IS NOT NULL
+AND bin in (
+	SELECT distinct bin FROM facdb
+	WHERE datasource = 'dohmh_daycare'
+);
+
+-- For factype NYCHA COMMUNITY CENTER - SENIOR CENTER,
+-- If dfta_contracts has a site with the same BIN, delete the nycha record
+DELETE FROM facdb
+WHERE factype = 'NYCHA COMMUNITY CENTER - SENIOR CENTER'
+AND datasource = 'nycha_communitycenters'
+AND bin IS NOT NULL
+AND bin in (
+	SELECT distinct bin FROM facdb
+	WHERE datasource = 'dfta_contracts'
+);
