227d52c5227a5eb9b688ccfaf323c7d34ddb45a9
sptkl <caobaiyue@gmail.com>
2021-06-11 18:14:08 +0000

    uncomment code

--- facdb/bash/dataloading.sh
+++ facdb/bash/dataloading.sh
@@ -3,16 +3,16 @@ CURRENT_DIR=$(dirname "$(readlink -f "$0")")
 source $CURRENT_DIR/config.sh
 max_bg_procs 5
 
-# import_public dcp_facilities &
+import_public dcp_facilities &
 import_public dcp_mappluto &
-# import_public dcp_boroboundaries_wi &
-# import_public dcp_censustracts &
-# import_public dcp_councildistricts &
-# import_public dcp_cdboundaries &
-# import_public dcp_ntaboundaries &
-# import_public dcp_policeprecincts &
-# import_public doitt_zipcodeboundaries &
-# import_public doitt_buildingfootprints &
-# import_public dcp_school_districts &
+import_public dcp_boroboundaries_wi &
+import_public dcp_censustracts &
+import_public dcp_councildistricts &
+import_public dcp_cdboundaries &
+import_public dcp_ntaboundaries &
+import_public dcp_policeprecincts &
+import_public doitt_zipcodeboundaries &
+import_public doitt_buildingfootprints &
+import_public dcp_school_districts &
 
 wait
