1ac49b76ff3ef1d9d4c1bfe61ca51bdfb44af58a
Alexander Weinstein <aweinstein@planning.nyc.gov>
2022-05-17 16:43:45 -0400

    load dump metadata from utlity

--- facdb/__init__.py
+++ facdb/__init__.py
@@ -4,4 +4,5 @@ from .geocode.functionBN import FunctionBN
 from .geocode.parseAddress import ParseAddress, UseAirportName
 from .utility.executeSQL import ExecuteSQL
 from .utility.export import Export
+from .utility.metadata import dump_metadata
 from .utility.prepare import Prepare
