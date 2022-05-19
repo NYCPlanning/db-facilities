195b52d18136367ddec9cd06b3895c1055d0c0de
Alexander Weinstein <aweinstein@planning.nyc.gov>
2022-05-19 12:04:33 -0400

    add echos to export step

--- facdb/bash/export.sh
+++ facdb/bash/export.sh
@@ -3,22 +3,25 @@ CURRENT_DIR=$(dirname "$(readlink -f "$0")")
 source $CURRENT_DIR/config.sh
 max_bg_procs 5
 
-mkdir -p output && (
-    cd output
-    echo "*" > .gitignore
-    CSV_export facdb facilities &
-    CSV_export qc_operator &
-    CSV_export qc_oversight &
-    CSV_export qc_classification &
-    CSV_export qc_captype &
-    CSV_export qc_mapped &
-    CSV_export qc_diff &
-    CSV_export qc_recordcounts &
-    CSV_export qc_subgrpbins &
-    # CSV_export geo_rejects &
-    # CSV_export geo_result &
-    SHP_export facdb POINT facilities
-    FGDB_export facdb POINT facilities
-    wait
-    echo "export complete"
-)
+# mkdir -p output && (
+echo "current directory is ${CURREMT_DIR}"
+echo "contents of output"
+find /output -type f -printf "%f\n"
+cd output
+echo "*" > .gitignore
+CSV_export facdb facilities &
+CSV_export qc_operator &
+CSV_export qc_oversight &
+CSV_export qc_classification &
+CSV_export qc_captype &
+CSV_export qc_mapped &
+CSV_export qc_diff &
+CSV_export qc_recordcounts &
+CSV_export qc_subgrpbins &
+# CSV_export geo_rejects &
+# CSV_export geo_result &
+SHP_export facdb POINT facilities
+FGDB_export facdb POINT facilities
+wait
+echo "export complete"
+# )
