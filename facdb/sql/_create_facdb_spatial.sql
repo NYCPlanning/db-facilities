6a26ce46869213b0bf3301a4486226d23fae6521
Finn van Krieken <fvankrieken@planning.nyc.gov>
2023-04-10 14:55:54 -0400

    Move logic around joining all council districts to sql

--- facdb/sql/_create_facdb_spatial.sql
+++ facdb/sql/_create_facdb_spatial.sql
@@ -10,13 +10,14 @@ with boundary_geosupport as (
 		nullif(geo_1b->'result'->>'geo_commboard','') as commboard,
 		nullif(geo_1b->'result'->>'geo_nta2010','') as nta2010,
 		nullif(geo_1b->'result'->>'geo_nta2020','') as nta2020,
-		nullif(geo_1b->'result'->>'geo_council','') as council,
+		coundist::text as council, --nullif(geo_1b->'result'->>'geo_council','') as council,
 		nullif(geo_1b->'result'->>'geo_ct2010','000000') as ct2010,
 		nullif(geo_1b->'result'->>'geo_ct2020','000000') as ct2020,
 		nullif(geo_1b->'result'->>'geo_policeprct','') as policeprct,
 		nullif(geo_1b->'result'->>'geo_schooldist','') as schooldist,
 		'geosupport' as boundarysource
 	FROM facdb_base
+	LEFT JOIN dcp_councildistricts b ON st_intersects(b.wkb_geometry, facdb_base.geom)
 	WHERE nullif(geo_1b->'result'->>'geo_grc','') IN ('00', '01')
 	AND nullif(geo_1b->'result'->>'geo_grc2','') IN ('00', '01')
 ), boundary_spatial_join as (
